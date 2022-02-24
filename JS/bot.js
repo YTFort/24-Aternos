ab72aecedcb844e7d4e8c44bee1b1f6cfe8269fb
YTFort <100344180+YTFort@users.noreply.github.com>
2022-02-24 11:48:22 +0300

    Add files via upload

---
+++ JS/bot.js
@@ -0,0 +1,40 @@
+const mineflayer = require('mineflayer');
+
+console.log('Starting...')
+
+function createBot () {
+    const bot = mineflayer.createBot({
+    host: "localhost",
+    port: "25565",
+    username: "Test",
+    version: false
+    })
+    bot.on('login', function() {
+      bot.chat('/reginster 123123123 123123123')
+    })
+    bot.on('chat', (username, message) => {
+      if (username === bot.username) return
+      target = bot.players[username].entity
+      let entity
+      switch (message) {
+        case ';start':
+          bot.setControlState('forward', true)
+          bot.setControlState('jump', true)
+          bot.setControlState('sprint', true)
+          break
+          case ';stop':
+            bot.clearControlStates()
+            break
+          }
+        })
+        bot.on('spawn', function() {
+          bot.chat('Spawned')
+        })
+        bot.on('death', function() {
+          bot.chat('I died, respawn')
+        })
+        bot.on('kicked', (reason, loggedIn) => console.log(reason, loggedIn))
+        bot.on('error', err => console.log(err))
+        bot.on('end', createBot)
+}
+createBot()
