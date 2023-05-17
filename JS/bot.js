cd1a0025d4c43d0b554c470a4ada298185e280fd
Dabest2505 <133912115+Dabest2505@users.noreply.github.com>
2023-05-17 14:18:03 -0500

    Update bot.js

--- JS/bot.js
+++ JS/bot.js
@@ -4,8 +4,8 @@ console.log('Starting...')
 
 function createBot () {
     const bot = mineflayer.createBot({
-    host: "localhost",
-    port: "25565",
+    host: "tenson.aternos.me",
+    port: "31719",
     username: "Test",
     version: false
     })
