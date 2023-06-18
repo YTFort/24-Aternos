bd92acb6b0753c168887f216dbf93015a7cd67ad
Fortcote <100344180+YTFort@users.noreply.github.com>
2023-06-19 00:12:35 +0300

    V2

--- JS/bot.js
+++ JS/bot.js
@@ -6,32 +6,33 @@ function createBot () {
     const bot = mineflayer.createBot({
     host: "localhost",
     port: "25565",
-    username: "Test",
+    username: "24ATERNOSBOT",
     version: false
     })
     bot.on('login', function() {
       bot.chat('/reginster 123123123 123123123')
+      bot.chat('/login 123123123 123123123')
     })
     bot.on('chat', (username, message) => {
       if (username === bot.username) return
-      target = bot.players[username].entity
-      let entity
       switch (message) {
         case ';start':
+          bot.chat('24 ATERNOS > Bot started! - Made By Fortcote')
           bot.setControlState('forward', true)
           bot.setControlState('jump', true)
           bot.setControlState('sprint', true)
           break
           case ';stop':
+            bot.chat('24 ATERNOS > Bot stoped! - Made By Fortcote')
             bot.clearControlStates()
             break
           }
         })
         bot.on('spawn', function() {
-          bot.chat('Spawned')
+          bot.chat('Bot > Spawned')
         })
         bot.on('death', function() {
-          bot.chat('I died, respawn')
+          bot.chat('Bot > I died, respawn')
         })
         bot.on('kicked', (reason, loggedIn) => console.log(reason, loggedIn))
         bot.on('error', err => console.log(err))
